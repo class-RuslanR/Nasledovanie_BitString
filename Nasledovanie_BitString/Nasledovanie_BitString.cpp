9f66f9d1e855d428a1d91600b0934e93bdf4ba4f
Ruslan Rogozhkin <57987419+class-RuslanR@users.noreply.github.com>
2020-01-28 09:56:45 +0200

    N01.Описал базовый класс полностью Описал производный класс(конструктор, деструктор,  изменение знака на противоположный) В работе

--- Nasledovanie_BitString/Nasledovanie_BitString.cpp
+++ Nasledovanie_BitString/Nasledovanie_BitString.cpp
@@ -1,34 +1,134 @@
 ﻿#include "pch.h"
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
 class String
 {
 protected:
-	string str;
+	char *str;
+	int length;
 public:
-	String(string str);
+	String();
+	String(char *strLiter);
+	String(char symbol);
+	int GetLength_String();
+	void ClearString();
+	void ShowString();
 	~String();
 };
 
-class StringBit : public String
+String::String()
 {
+	str = 0;
+}
 
-};
-
-String::String(string str)
+String::String(char * strLiter)
 {
-	this->str = str;
-	int length = strlen(str);
-	string *str = new string[length];
+	length = strlen(strLiter);
+	str = new char[length];
+	strcpy(str, strLiter);
+}
+
+String::String(char symbol)
+{
+	str = new char;
+	str[0] = symbol;
+	str[1] = '\0';
+	length = 1;
+}
+
+int String::GetLength_String()
+{
+	return length;
+}
+
+void String::ClearString()
+{
+	delete str;
+	length = 0;
+	str = 0;
+}
+
+void String::ShowString()
+{
+	cout << str << "\n\n";
 }
 
 String::~String()
 {
-	delete str;
+	if (str)
+	{
+		delete str;
+		str = 0;
+	}
 }
+
+class StringBit : public String
+{
+public:
+	StringBit();
+	StringBit(char*strLiter) : String(strLiter)
+	{
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			if (str[i] != '0' && str[i] != '1')
+			{
+				str[i] = '\0';
+			}
+		}
+	}
+	~StringBit();
+	char * changeNeg_or_Pos();
+	StringBit operator + (StringBit & str2);
+};
+
+StringBit::StringBit()
+{
+	String();
+}
+
+StringBit::~StringBit()
+{
+	String ::~String();
+}
+
+char * StringBit::changeNeg_or_Pos()
+{
+
+	if (str[0] == '-')
+	{
+		for (int i = 0; str[i] <= length; i++)
+		{
+			str[i] = str[i + 1];
+		}
+		length -= 1;
+		str = new char[length];
+		return str;
+	}
+	else if (str[0] == '0' || str[0] == '1')
+	{
+		length += 1;
+		str = new char[length];
+		for (int i = length; str[i] >= 0; i--)
+		{
+			str[i] = str[i - 1];
+			if (str[0])
+			{
+				str[0] = '-';
+			}
+		}
+		return str;
+	}
+}
+
+StringBit StringBit::operator+(StringBit & str2)
+{
+	return StringBit();
+}
+
+
 
 int main()
 {
@@ -39,3 +139,5 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
